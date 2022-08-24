12d5147f4b0cb277fb502a0a756f0b52f975f957
Sergey <mulichev@gmail.com>
2022-08-24 17:56:17 +0300

    set view to activity_main

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_edit_item)
+        setContentView(R.layout.activity_main)
 
     }
 }
