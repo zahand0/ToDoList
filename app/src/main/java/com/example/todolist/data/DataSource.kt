a1ed79e27181d391daa2c86f685a2eb2786fbe4b
Sergey <mulichev@gmail.com>
2022-08-24 18:04:10 +0300

    hardcoded data for testing purposes

---
+++ app/src/main/java/com/example/todolist/data/DataSource.kt
@@ -0,0 +1,10 @@
+package com.example.todolist.data
+
+object DataSource {
+
+    val todoItems = arrayOf(
+        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("02", "Task 02", TaskPriority.URGENT, false, "03-09-2022", "24-08-2022", "24-08-2022")
+
+    )
+}
