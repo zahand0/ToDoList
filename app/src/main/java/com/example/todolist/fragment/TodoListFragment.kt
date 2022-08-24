f7c918417aa75cb1476fd9159cc965ed09e4d46a
Sergey <mulichev@gmail.com>
2022-08-24 18:01:57 +0300

    added binding, set up adapter, added shared viewmodel

--- app/src/main/java/com/example/todolist/fragment/TodoListFragment.kt
+++ app/src/main/java/com/example/todolist/fragment/TodoListFragment.kt
@@ -1,15 +1,22 @@
-package com.example.todolist.ui
+package com.example.todolist.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.adapter.TodoListAdapter
 import com.example.todolist.databinding.FragmentTodoListBinding
+import com.example.todolist.viewmodel.TodoItemsViewModel
 
 class TodoListFragment: Fragment() {
 
      private var binding: FragmentTodoListBinding? = null
+
+     private val viewModel: TodoItemsViewModel by activityViewModels()
 
      override fun onCreateView(
           inflater: LayoutInflater,
@@ -19,5 +26,25 @@ class TodoListFragment: Fragment() {
           val fragmentBinding = FragmentTodoListBinding.inflate(inflater, container, false)
           binding = fragmentBinding
           return fragmentBinding.root
+     }
+
+     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+          super.onViewCreated(view, savedInstanceState)
+
+          val adapter = TodoListAdapter {
+               val action = TodoListFragmentDirections.actionTodoListFragmentToEditItemFragment(it.id)
+               this.findNavController().navigate(action)
+          }
+
+          adapter.submitList(viewModel.allItems)
+          binding?.recyclerView?.adapter = adapter
+
+          binding?.recyclerView?.layoutManager = LinearLayoutManager(this.context)
+          binding?.addNewTask?.setOnClickListener {
+               val action = TodoListFragmentDirections.actionTodoListFragmentToEditItemFragment(
+                    "03"
+               )
+               this.findNavController().navigate(action)
+          }
      }
 }
