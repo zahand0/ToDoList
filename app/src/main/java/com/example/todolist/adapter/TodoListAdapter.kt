d6a07f3dc7c5fd21b9d6bf97da83679b253cd4af
Sergey <mulichev@gmail.com>
2022-08-24 18:03:30 +0300

    added todo list recycler view adapter

---
+++ app/src/main/java/com/example/todolist/adapter/TodoListAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.todolist.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.data.TaskPriority
+import com.example.todolist.data.TodoItem
+import com.example.todolist.databinding.TodoItemBinding
+
+class TodoListAdapter(private val onItemClicked: (TodoItem) -> Unit) : ListAdapter<TodoItem, TodoListAdapter.ItemViewHolder>(DiffCallBack) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            TodoItemBinding.inflate(LayoutInflater.from(parent.context))
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+        holder.bind(current)
+    }
+
+    class ItemViewHolder(private val binding: TodoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: TodoItem) {
+            binding.taskDate.text = item.deadlineDate
+            binding.taskDescription.text = item.description
+            binding.taskStatus.isChecked = item.isDone
+            binding.taskImportance.text = when (item.priority) {
+                TaskPriority.URGENT -> "!!"
+                else -> ""
+            }
+        }
+    }
+
+    companion object {
+        private val DiffCallBack = object : DiffUtil.ItemCallback<TodoItem>() {
+            override fun areItemsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: TodoItem, newItem: TodoItem): Boolean {
+                return oldItem.description == newItem.description &&
+                        oldItem.isDone == newItem.isDone &&
+                        oldItem.deadlineDate == newItem.deadlineDate &&
+                        oldItem.priority == newItem.priority
+            }
+
+        }
+    }
+
+}
