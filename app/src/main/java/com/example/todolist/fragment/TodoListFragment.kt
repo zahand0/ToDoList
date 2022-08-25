09eb4255b1c7e5940d4c04f855069811403bc8f0
Sergey <mulichev@gmail.com>
2022-08-25 14:19:10 +0300

    added action on eye button

--- app/src/main/java/com/example/todolist/fragment/TodoListFragment.kt
+++ app/src/main/java/com/example/todolist/fragment/TodoListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -45,6 +46,10 @@ class TodoListFragment: Fragment() {
                     "03"
                )
                this.findNavController().navigate(action)
+          }
+
+          binding?.showDoneTasks?.setOnClickListener {
+               Toast.makeText(requireContext(), "Hide done tasks",Toast.LENGTH_SHORT).show()
           }
      }
 }
