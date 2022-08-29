5aaf79d68f0f03d68361d2b98bf7de5b82094b86
Sergey <mulichev@gmail.com>
2022-08-29 21:36:43 +0300

    more data

--- app/src/main/java/com/example/todolist/data/DataSource.kt
+++ app/src/main/java/com/example/todolist/data/DataSource.kt
@@ -3,23 +3,28 @@ package com.example.todolist.data
 object DataSource {
 
     val todoItems = arrayOf(
+        TodoItem("01", "Task 01", TaskPriority.NORMAL, false, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("02", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("03", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("04", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("05", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("06", "Tasksdg nfkj fa s asdf asdfasd  asdf asd asdf asd fasd asdf as as dfdlngsjkdfngkj  01", TaskPriority.NORMAL, false, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("02", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("03", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("04", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("05", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("06", "Tasksdg nfkj fa s asdf asdfasd  asdf asd asdf asd fasd asdf as as dfdlngsjkdfngkj  01", TaskPriority.NORMAL, false, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("07", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.LOW, true, "07-07-2022", "07-07-2021", "09-08-2022"),
+        TodoItem("01", "Task 01", TaskPriority.NORMAL, false, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
         TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("01", "Task 01", TaskPriority.NORMAL, true, "07-07-2022", "07-07-2021", "09-08-2022"),
-        TodoItem("02", "Task 02", TaskPriority.URGENT, false, "03-09-2022", "24-08-2022", "24-08-2022")
+        TodoItem("09", "Task 02", TaskPriority.URGENT, false, "03-09-2022", "24-08-2022", "24-08-2022")
 
     )
 }
