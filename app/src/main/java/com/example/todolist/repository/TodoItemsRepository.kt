c259ec9dd4d9c84cc1036c13037f50a7cd72d6e5
Sergey <mulichev@gmail.com>
2022-08-30 20:11:23 +0300

    added function to add items

--- app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
+++ app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
@@ -1,10 +1,17 @@
 package com.example.todolist.repository
 
+import com.example.todolist.data.CommonDateFormats
 import com.example.todolist.data.DataSource
 import com.example.todolist.data.TodoItem
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class TodoItemsRepository {
 
-    val todoItems: List<TodoItem> = DataSource.todoItems.toList()
+    val todoItems: List<TodoItem> = DataSource.todoItems
+
+    fun addItem(item: TodoItem) {
+        DataSource.addItem(item)
+    }
 }
