0ddfea18dd79f43712d33cd908bbde93a1d712f6
Sergey <mulichev@gmail.com>
2022-08-31 18:49:24 +0300

    items now store in stateflow

--- app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
+++ app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
@@ -3,13 +3,16 @@ package com.example.todolist.repository
 import com.example.todolist.data.CommonDateFormats
 import com.example.todolist.data.DataSource
 import com.example.todolist.data.TodoItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asFlow
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class TodoItemsRepository {
 
-    val todoItems: List<TodoItem> = DataSource.todoItems
+    val todoItems: StateFlow<List<TodoItem>> = DataSource.todoItems
 
     fun addItem(item: TodoItem) {
         DataSource.addItem(item)
