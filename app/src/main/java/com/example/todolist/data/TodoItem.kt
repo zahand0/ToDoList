c328833615d3c0a8c72ee526c3c1f06dcac99d7f
Sergey <mulichev@gmail.com>
2022-09-07 15:42:43 +0300

    transform to room entity, id type changed from string to int

--- app/src/main/java/com/example/todolist/data/TodoItem.kt
+++ app/src/main/java/com/example/todolist/data/TodoItem.kt
@@ -1,11 +1,24 @@
 package com.example.todolist.data
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
 data class TodoItem(
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name = "description")
     val description: String,
+    @ColumnInfo(name = "priority")
     val priority: TaskPriority,
+    @ColumnInfo(name = "is_done")
     val isDone: Boolean,
+    @ColumnInfo(name = "deadline_date")
     val deadlineDate: Long?,
+    @ColumnInfo(name = "creation_date")
     val creationDate: Long,
+    @ColumnInfo(name = "last_edit_date")
     val lastEditDate: Long
 )
