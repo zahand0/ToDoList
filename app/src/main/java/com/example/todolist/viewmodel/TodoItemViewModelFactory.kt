9b143dce51ef36159e9bfac98638b5acf65f35bf
Sergey <mulichev@gmail.com>
2022-09-08 15:37:41 +0300

    created viewModel factory

---
+++ app/src/main/java/com/example/todolist/viewmodel/TodoItemViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.todolist.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class TodoItemViewModelFactory(val app: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TodoItemsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TodoItemsViewModel(app) as T
+        }
+        throw IllegalArgumentException("Unable to construct viewmodel")
+    }
+}
