0d4efa0aefbcbfff04a17f14f925c4c201d8f35f
Sergey <mulichev@gmail.com>
2022-09-20 20:09:45 +0300

    added service to send task list and get updated task list

---
+++ app/src/main/java/com/example/todolist/network/api/TodoListApiService.kt
@@ -0,0 +1,12 @@
+package com.example.todolist.network.api
+
+import com.example.todolist.network.NetworkItemContainer
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface TodoListApiService {
+    @Headers("token: 12345678")
+    @POST("get_all_tasks")
+    suspend fun updateTaskList(@Body container: NetworkItemContainer): NetworkItemContainer
+}
