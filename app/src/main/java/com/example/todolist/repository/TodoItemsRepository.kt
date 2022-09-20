3c0935b1b5ed91dfddd2aae608f8e2304ced1fb4
Sergey <mulichev@gmail.com>
2022-09-20 20:06:53 +0300

    added refreshItems function to send all data from database and get updated items

--- app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
+++ app/src/main/java/com/example/todolist/repository/TodoItemsRepository.kt
@@ -1,12 +1,15 @@
 package com.example.todolist.repository
 
 import android.util.Log
-import com.example.todolist.data.DataSource
 import com.example.todolist.data.TodoItem
+import com.example.todolist.data.asNetworkItem
 import com.example.todolist.data.database.ItemDatabase
+import com.example.todolist.network.NetworkItemContainer
+import com.example.todolist.network.api.TodoListApi.api
+import com.example.todolist.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 
 
@@ -43,10 +46,31 @@ class TodoItemsRepository(private val database: ItemDatabase) {
         }
     }
 
-    suspend fun getNumberDoneItems():Flow<Int> {
+    suspend fun getNumberDoneItems(): Flow<Int> {
         return withContext(Dispatchers.IO) {
             dao.getNumberDoneItems()
         }
     }
 
+    suspend fun refreshItems() {
+        try {
+            withContext(Dispatchers.IO) {
+                Log.d("repository", "sss")
+
+                val taskList = todoItems.first().map { it.asNetworkItem() }
+                Log.d("repository", taskList[0].description)
+
+                val updatedTasks =
+                    api.updateTaskList(NetworkItemContainer(taskList)).asDatabaseModel()
+
+                Log.d("repository", updatedTasks.toString())
+                for (task in updatedTasks) {
+                    updateItem(task)
+                }
+
+            }
+        } catch (e: Error) {
+            Log.d("repository", e.message ?: "message")
+        }
+    }
 }
