20b4b65e4576e830806835f6049559f2ba71fdb6
Sergey <mulichev@gmail.com>
2022-10-03 16:12:48 +0300

    added RepositoryComponent

--- app/src/main/java/com/example/todolist/TodoListApplication.kt
+++ app/src/main/java/com/example/todolist/TodoListApplication.kt
@@ -2,8 +2,19 @@ package com.example.todolist
 
 import android.app.Application
 import com.example.todolist.data.database.ItemDatabase
+import com.example.todolist.di.AppComponent
+import com.example.todolist.di.DaggerRepositoryComponent
+import com.example.todolist.di.RepositoryComponent
 
 class TodoListApplication: Application() {
 
-    val database: ItemDatabase by lazy { ItemDatabase.getDatabase(this) }
+    private lateinit var repositoryComponent: RepositoryComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        repositoryComponent = DaggerRepositoryComponent.factory().create(this.applicationContext)
+    }
+
+    fun getRepositoryComponent(): RepositoryComponent = repositoryComponent
+
 }
