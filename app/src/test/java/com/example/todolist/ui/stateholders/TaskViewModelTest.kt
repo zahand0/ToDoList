fd71865fdf372f542c82b766df48ee8907ff0ede
Sergey <mulichev@gmail.com>
2022-10-17 11:10:07 +0300

    testing TaskViewModel

--- app/src/test/java/com/example/todolist/ui/stateholders/TaskViewModelTest.kt
+++ app/src/test/java/com/example/todolist/ui/stateholders/TaskViewModelTest.kt
@@ -1,5 +1,94 @@
 package com.example.todolist.ui.stateholders
 
-import org.junit.Assert.*
+import android.util.Log
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.todolist.MainCoroutineRule
+import com.example.todolist.data.TaskModel
+import com.example.todolist.data.TaskPriority
+import com.example.todolist.network.exception.NetworkState
+import com.example.todolist.repository.FakeTaskRepository
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.internal.artificialFrame
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-class TaskViewModelTest
+@ExperimentalCoroutinesApi
+class TaskViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: TaskViewModel
+    private lateinit var repository: FakeTaskRepository
+
+    @Before
+    fun setup() {
+        repository = FakeTaskRepository()
+        viewModel = TaskViewModel(repository)
+    }
+
+    @Test
+    fun `add item works`() = runTest {
+        viewModel.addItem(
+            "",
+            TaskPriority.NORMAL,
+            false,
+            null,
+            1,
+            1
+        )
+        assertThat(viewModel.allItems.first()).isNotEmpty()
+    }
+
+
+    @Test
+    fun `allItems is correct`() = runTest {
+        viewModel.addItem(
+            "",
+            TaskPriority.NORMAL,
+            false,
+            null,
+            1,
+            1
+        )
+        viewModel.addItem(
+            "",
+            TaskPriority.NORMAL,
+            false,
+            null,
+            1,
+            1
+        )
+        assertThat(viewModel.allItems.first()).hasSize(2)
+    }
+
+    @Test
+    fun `refreshItems returns OK when NetworkState is OK`() = runTest {
+        repository.setNetworkState(NetworkState.OK)
+        viewModel.refreshItems()
+        assertThat(viewModel.lastRefreshStatus).isEqualTo(NetworkState.OK)
+    }
+
+    @Test
+    fun `refreshItems returns UNDEFINED_ERROR when NetworkState is UNDEFINED_ERROR`() = runTest {
+        repository.setNetworkState(NetworkState.UNDEFINED_ERROR)
+        viewModel.refreshItems()
+        assertThat(viewModel.lastRefreshStatus).isEqualTo(NetworkState.UNDEFINED_ERROR)
+    }
+
+    @Test
+    fun `refreshItems returns CONNECTION_ERROR when NetworkState is CONNECTION_ERROR`() = runTest {
+        repository.setNetworkState(NetworkState.CONNECTION_ERROR)
+        viewModel.refreshItems()
+        assertThat(viewModel.lastRefreshStatus).isEqualTo(NetworkState.CONNECTION_ERROR)
+    }
+
+}
